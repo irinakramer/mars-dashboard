442b2e09bf504c7936520f6cd77531269fa55af7
Irina Kramer <irakramer@gmail.com>
2021-02-04 16:35:50 -0800

    feat: Added fetch urls Spirit and Opportunity

--- src/server/index.js
+++ src/server/index.js
@@ -14,15 +14,28 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
 
-app.get('/curiosity', async (req, res) => {
-    const name = req.params.id;
-    //TODO - need dates and URLs for spirit and opportunity with multiple photos
-    //  and replace curiosity with {name}
+app.get('/:name', async (req, res) => {
+    const name = req.params.name.toLowerCase();
+    let date;
+    let url;
+    if (name === 'opportunity') {
+        date = '2014-07-13'
+        url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
+    } else if (name === 'spirit') {
+        date = '2005-11-30'
+        url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`
+    } else if (name === 'curiosity') {
+        url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/latest_photos?api_key=${process.env.API_KEY}`
+    } else {
+        res.send('Invalid address.')
+        return
+    }
+
     try {
-        let data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?&api_key=${process.env.API_KEY}`)
+        const data = await fetch(url)
             .then(res => res.json())
         res.send({ data })
-        console.log(req.params);
+        console.log(data)
     } catch (err) {
         console.log('error:', err);
     }
